b547f5d8c7aae9a727243613cfed2a6c4aaa4d82
Ryan Freng <ryanfreng@gmail.com>
2014-07-30 22:51:34 -0500

    finish 3.5, enable postgres locally and on test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  let(:base_title) { "Filmwin" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -11,7 +13,7 @@ describe "Static Pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Filmwin | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   
   end
@@ -25,7 +27,7 @@ describe "Static Pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Filmwin | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
 
   end
@@ -39,9 +41,24 @@ describe "Static Pages" do
 
     it "should have the title 'About'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Filmwin | About")
+      expect(page).to have_title("#{base_title} | About")
     end
 
   end 
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+  
+  end
+
 
 end

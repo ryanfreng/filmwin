85c29b784f0820307f382aca14b87890bda8162c
Ryan Freng <ryanfreng@gmail.com>
2014-08-01 23:27:59 -0500

    create user index page with pagination

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name      "Ryan Freng"
-    email     "ryan@backflipfilms.com"
-    password  "foobar"
+    sequence(:name)   { |n| "Person #{n}" }
+    sequence(:email)  { |n| "person_#{n}@example.com"}
+    password "foobar"
     password_confirmation "foobar"
   end
 end

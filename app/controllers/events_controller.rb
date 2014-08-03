3162167c6bff400c194a3120a6adb77343335b89
Ryan Freng <ryanfreng@gmail.com>
2014-08-03 02:01:21 -0500

    finish basic event implementation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
-  before_action :signed_in_user,  only: [:new, :edit]
+  before_action :signed_in_user,  only: [:new, :edit, :update]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def new
   end
@@ -8,7 +9,30 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def update
+    @event = Event.find(params[:id])
+    if @event.update_attributes(event_params)
+      flash[:success] = "Successfully updated event"
+      redirect_to @event
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @event = Event.find(params[:id])
   end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:name, :sub_heading, :description,
+                                    :entry_start_date, :entry_end_date,
+                                    :judging_date, :show_date)
+    end
+
+    def correct_user
+      @event = current_user.events.find_by(id: params[:id])
+      redirect_to root_url if @event.nil?
+    end
 end
--- spec/requests/event_pages_spec.rb
+++ spec/requests/event_pages_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 
 describe 'Event pages' do
   
-  #let(:user)  { FactoryGirl.create(:user) }
-  #let(:event) { FactoryGirl.create(:event, created_by: user.id) }
   let(:event) { FactoryGirl.create(:event) }
 
   subject { page }
@@ -31,6 +29,60 @@ describe 'Event pages' do
       visit edit_event_path(event)
     end
 
-    it { should have_content("Update your event") }
+    describe "page" do
+      it { should have_content("Update your event") }
+      ['Name', 'Sub heading', 'Description', 'Entry start date',
+                'Entry end date', 'Judging date', 'Show date'].each do |n|
+        it { should have_content(n) }
+      end
+    end
+
+    describe "with invalid information" do
+      before do
+        fill_in "Entry start date",    with: 'abcd'
+        click_button 'Save changes'
+      end
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name)              { 'New Wave Awards' }
+      let(:new_entry_start_date)  { Date.today }
+      let(:new_sub_heading)       { Faker::Lorem.sentence(5) }
+
+      before do
+        fill_in "Name",               with: new_name
+        fill_in "Entry start date",   with: new_entry_start_date
+        fill_in "Sub heading",        with: new_sub_heading
+        click_button "Save changes"
+      end
+
+      it { should_not have_content('error') }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Edit') }
+      specify { expect(event.reload.name).to eq new_name }
+      specify { expect(event.reload.entry_start_date).to eq new_entry_start_date }
+      specify { expect(event.reload.sub_heading).to eq new_sub_heading }
+
+    end
+
+    describe "forbidden attributes" do
+      let(:params) do
+        { event: { user_id: 3, name: event.name, sub_heading: event.sub_heading,
+                    description: event.description, entry_start_date: event.entry_start_date,
+                    entry_end_date: event.entry_end_date, judging_date: event.judging_date,
+                    show_date: event.show_date } }
+      end
+
+      before do
+        sign_in event.user, no_capybara: true
+        patch event_path(event), params
+      end
+
+      specify { expect(event.reload).not_to eq 3 }
+
+    end
+
   end
 end

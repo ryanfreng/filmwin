783d955a818f669f3905738167970880d9bf7e19
Ryan Freng <ryanfreng@gmail.com>
2014-08-16 23:32:57 -0500

    fix params

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,24 +3,28 @@ class OrdersController < ApplicationController
 
   def create
     if params.has_key?(:txn_id)
-      response = validate_IPN_notification(request.raw_post)
+      if ENV['ENVIRONMENT'] == 'Development'
+        response = "VERIFIED"
+      else
+        response = validate_IPN_notification(request.raw_post)
+      end
       logger.info("Here's the response: #{response}")
       case response
       when "VERIFIED"
-        order = Order.new(txn_id: params(:txn_id),
-                  email: params(:payer_email),
-                  verify_sign: params(:verify_sign),
-                  first_name: params(:first_name),
-                  last_name: params(:last_name),
-                  address_city: params(:address_city),
-                  address_name: params(:address_name),
-                  address_state: params(:address_state),
-                  address_street: params(:address_street),
-                  address_zip: params(:address_zip),
-                  payment_gross: params(:payment_gross),
-                  payment_status: params(:payment_status))
+        order = Order.new(txn_id: params[:txn_id],
+                  email: params[:payer_email],
+                  verify_sign: params[:verify_sign],
+                  first_name: params[:first_name],
+                  last_name: params[:last_name],
+                  address_city: params[:address_city],
+                  address_name: params[:address_name],
+                  address_state: params[:address_state],
+                  address_street: params[:address_street],
+                  address_zip: params[:address_zip],
+                  payment_gross: params[:payment_gross],
+                  payment_status: params[:payment_status])
         if order.save
-          submissions = Submission.find(/ids=\[(.*)\]/.match(params(:item_name))[1].split(',') )
+          submissions = Submission.find(/ids=\[(.*)\]/.match(params[:item_name])[1].split(',') )
           submissions.each do |s|
             s.update_attributes(order: order)
           end

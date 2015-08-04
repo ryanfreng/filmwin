158bd921ef2d94ff6c307ff7bae65fd0e76bd4a1
Ryan Freng <ryanfreng@gmail.com>
2015-08-04 18:15:06 -0500

    fix sorts

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,7 +3,7 @@ class AdminController < ApplicationController
   before_action :admin_user,      only: [:index]
 
   def index
-    @events = Event.all
+    @events = Event.all.sort {|a, b| b.show_date <=> a.show_date }
   end
 
 end
